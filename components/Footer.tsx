8b95157df6dad55827536b61fc1536bee4ee540e
jakeevans <ejake370@gmail.com>
2024-10-19 17:11:12 -0600

    Create form to capture user input

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <>
+      <div className="text-gray-500 text-sm ">Jake Evans @ 2024</div>
+    </>
+  );
+}
---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <>
+      <div className="flex justify-end">
+        <ul>
+          <li>View on Github</li>
+        </ul>
+      </div>
+    </>
+  );
+}
